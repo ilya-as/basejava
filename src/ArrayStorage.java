5feec4cf61ce9fdcdee3a9cb27312a07a3127c0a
Ilya <aslamovi@gmail.com>
2018-06-15 20:50:35 +0300

    решенное задание HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -28,12 +28,14 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-   	   for (int i = 0; i < size; i++) {
-		if (storage[i].uuid.equals(uuid)) {
-		    System.arraycopy(storage, i+1, storage, i, size-i-1);
-		    size--;
-		}
-	    }
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size-1];
+                storage[size-1] = null;
+                size--;
+                return;
+            }
+        }
     }
 
     /**
