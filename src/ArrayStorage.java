a20c940b3ff680e59916143853acfd5cb5f47a30
ilya-as <40139351+ilya-as@users.noreply.github.com>
2018-06-15 16:35:45 +0300

    Update ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -30,7 +30,7 @@ public class ArrayStorage {
     void delete(String uuid) {
    	   for (int i = 0; i < size; i++) {
 		if (storage[i].uuid.equals(uuid)) {
-		    System.arraycopy(storage, i+1, storage, i, size-i);
+		    System.arraycopy(storage, i+1, storage, i, size-i-1);
 		    size--;
 		}
 	    }
