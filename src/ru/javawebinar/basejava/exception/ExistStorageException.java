5526862b92a9f5619d014a6dcab5eb7e060fea6d
ilya-as <aslamovi@gmail.com>
2018-08-15 09:33:36 +0300

    FileStorage

--- src/ru/javawebinar/basejava/exception/ExistStorageException.java
+++ src/ru/javawebinar/basejava/exception/ExistStorageException.java
@@ -3,5 +3,7 @@ package ru.javawebinar.basejava.exception;
 public class ExistStorageException extends StorageException {
     public ExistStorageException(String uuid) {
         super("Resume " + uuid + " already exist", uuid);
+
+
     }
 }
