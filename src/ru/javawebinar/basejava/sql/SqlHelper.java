2cec0a926b71d586cced5a773b4a1a9b65e3fe1c
ilya-as <aslamovi@gmail.com>
2018-10-22 13:36:07 +0300

    added class SQLHelper

--- src/ru/javawebinar/basejava/sql/SqlHelper.java
+++ src/ru/javawebinar/basejava/sql/SqlHelper.java
@@ -14,41 +14,13 @@ public class SqlHelper {
         this.connectionFactory = connectionFactory;
     }
 
-    public void executeQuery(String textQuery)  {
-        try {
-         PreparedStatement ps = prepareQuery(textQuery);
-            SqlRunner sqlRunner = (SqlRunner) ps;
-            executeQuery(textQuery, sqlRunner);
-        } catch (SQLException ex) {
-            throw parseException(ex);
-        }
-
-    }
-
-    public void executeQuery(String textQuery,SqlRunner sqlRunner) {
-        try {
-            sqlRunner.runQuery(prepareQuery(textQuery));
-        } catch (SQLException ex) {
-            throw parseException(ex);
-        }
-    }
-
-    public <T> T readRecords(String textQuery,SqlGetter sqlGetter) {
-
-        try {
-            return (T) sqlGetter.getRecords(prepareQuery(textQuery));
-        } catch (SQLException ex) {
-            throw parseException(ex);
-        }
-    }
-
-    private PreparedStatement prepareQuery(String textQuery) throws SQLException {
+    public PreparedStatement prepareQuery(String textQuery) throws SQLException {
         Connection conn = connectionFactory.getConnection();
         PreparedStatement ps = conn.prepareStatement(textQuery);
         return ps;
     }
 
-    private StorageException parseException(SQLException e) {
+    public StorageException parseException(SQLException e) {
         if (e.getSQLState().equals("23505")) {
             return new ExistStorageException(null);
         }
