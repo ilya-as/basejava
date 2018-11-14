1f4be5ae29a69a3477ab9e2d24b123c7b05a22eb
ilya-as <aslamovi@gmail.com>
2018-11-14 15:20:05 +0300

    lesson16

--- src/ru/javawebinar/basejava/model/ContactType.java
+++ src/ru/javawebinar/basejava/model/ContactType.java
@@ -2,12 +2,42 @@ package ru.javawebinar.basejava.model;
 
 public enum ContactType {
     TELEPHONE("Телефон: "),
-    SKYPE("Skype: "),
-    EMAIL("Почта: "),
-    LINDEDIN("Профиль Linkedin: "),
-    GITHUB("Профиль Github: "),
-    STACKOVERFLOW("Профиль Stackoverflow: "),
-    HOMEPAGE("Домашняя страница: ");
+    SKYPE("Skype: ") {
+        @Override
+        public String toHtml0(String value) {
+            return getTitle() + ": " + toLink("skype:" + value, value);
+        }
+    },
+    EMAIL("Почта: ") {
+        @Override
+        public String toHtml0(String value) {
+            return getTitle() + ": " + toLink("mailto:" + value, value);
+        }
+    },
+    LINDEDIN("Профиль Linkedin: ") {
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    GITHUB("Профиль Github: ") {
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    STACKOVERFLOW("Профиль Stackoverflow: ") {
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    HOMEPAGE("Домашняя страница: ") {
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    };
 
     private String title;
 
@@ -17,5 +47,21 @@ public enum ContactType {
 
     public String getTitle() {
         return title;
+    }
+
+    protected String toHtml0(String value) {
+        return title + ": " + value;
+    }
+
+    public String toHtml(String value) {
+        return (value == null) ? "" : toHtml0(value);
+    }
+
+    public String toLink(String href) {
+        return toLink(href, title);
+    }
+
+    public static String toLink(String href, String title) {
+        return "<a href='" + href + "'>" + title + "</a>";
     }
 }
